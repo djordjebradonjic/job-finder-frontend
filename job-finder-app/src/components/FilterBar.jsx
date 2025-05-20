75fd7919d3f7192d8b086c05c5c9c86f718f9d47
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-05-20 20:29:49 +0200

    project setup

--- job-finder-app/src/components/FilterBar.jsx
+++ job-finder-app/src/components/FilterBar.jsx
@@ -1,10 +1,23 @@
 
-function FilterBar(){
+function FilterBar(filters, setFilters, onSearch){
 
     return(
-        <div>
-
-        </div>
+    <div className="flex gap-2 mb-4">
+      <input
+        type="text"
+        placeholder="Search by title..."
+        value={filters.title}
+        onChange={(e) => setFilters({ ...filters, title: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Location..."
+        value={filters.location}
+        onChange={(e) => setFilters({ ...filters, location: e.target.value })}
+      />
+      <button onClick={onSearch}>Search</button>
+    </div>
+    
     );
 }
 
